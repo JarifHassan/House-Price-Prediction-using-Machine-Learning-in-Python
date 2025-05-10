ae455520b9e376bc923d17a19b8455c25e264227
JarifHassan <hassanjarif@gmail.com>
2025-05-11 02:10:06 +1200

    Update Code.py

--- Code.py
+++ Code.py
@@ -33,3 +33,12 @@ sns.heatmap (numerical_dataset.corr(),
                    linewidths = 2,
                    annot = True)
 
+unique_values = []
+for col in object_cols:
+    unique_values.append(dataset[col].unique().size)
+plt.figure(figsize=(10,6))
+plt.title('No. Unique values of Categorical Features')
+plt.xticks(rotation=90)
+sns.barplot(x=object_cols, y=unique_values)
+
+
